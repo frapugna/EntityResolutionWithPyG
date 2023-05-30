d120780516c6eb471d5fd0e1266fb269e61cdf63
jetixl <frapugna@gmail.com>
2023-05-30 16:42:09 +0200

    Fixed bugs with free entity resolution function

--- entity_resolution.py
+++ entity_resolution.py
@@ -18,11 +18,11 @@ def find_matching_couples_free(n_top_A, n_top_B, index_to_token, n_top):
                 if B[j] == k:
                     pos = j
                     break
-            if pos >= 0:
+            if pos >= 0 and k < A[i]:
                 matches.append((index_to_token[k], index_to_token[A[i]]))
-                matches_no_pos.append((index_to_token[k], index_to_token[A[i]]))
+                #matches_no_pos.append((index_to_token[k], index_to_token[A[i]]))
 
-    return matches, matches_no_pos
+    return set(matches)  #, matches_no_pos
 def one_to_one_clean_ER(dfpathA, dfpathB,n_epochs=100,p=20, q=1, n_similar=10, n_top=10, embedding_size=128, walk_length=10,context_size=10, use_faiss=True,file_directory=None, load_embedding_file=False, load_graph=False, load_n_best=False):
     """
         Params:
@@ -42,12 +42,11 @@ def one_to_one_clean_ER(dfpathA, dfpathB,n_epochs=100,p=20, q=1, n_similar=10, n
         - load_n_best: if true tells to load the embeddings from the file_directory skipping their computation
         Output: a set of couples represented the candidate matches found in the format (index, index+len(table_A))
     """
-    matches, n_pairs_before = entity_resolution(dfpathA, dfpathB, file_directory=None,p=p, q=q, n_epochs=n_epochs, n_similar=n_similar, n_top=n_top, embedding_size=embedding_size, walk_length=walk_length,context_size=context_size, use_faiss=use_faiss, load_embedding_file=load_embedding_file, load_graph=load_graph, load_n_best=load_n_best)
-    print(f'Old number of candidates: {n_pairs_before}\nNew number of candidates: {len(matches)}\nReduction ratio: {len(matches)/n_pairs_before} ')
+    matches = entity_resolution(dfpathA, dfpathB, file_directory=None,p=p, q=q, n_epochs=n_epochs, n_similar=n_similar, n_top=n_top, embedding_size=embedding_size, walk_length=walk_length,context_size=context_size, use_faiss=use_faiss, load_embedding_file=load_embedding_file, load_graph=load_graph, load_n_best=load_n_best)
     return matches
 
 
-def free_entity_resolution(df_list,file_directory=None,n_epochs=100,p=20, q=1, n_similar=10, n_top=10, embedding_size=128, walk_length=10,context_size=10, load_embedding_file=False, load_graph=False, load_n_best=False):
+def free_entity_resolution(df_list,file_directory=None,n_epochs=100,p=20, q=1, n_similar=10, n_top=10, embedding_size=128, walk_length=10,context_size=10, load_embedding_file=False, load_graph=False):
     """
         Params:
         - df_list: list of the path to the dataframe to process 
@@ -61,45 +60,45 @@ def free_entity_resolution(df_list,file_directory=None,n_epochs=100,p=20, q=1, n
         - file_directory: directory where to save the intermediate data
         - load_embedding_file: if true tells to load the embeddings from the file_directory skipping training
         - load_graph: if true tells to load the graph from the file_directory skipping its generation
-        - load_n_best: if true tells to load the embeddings from the file_directory skipping their computation
         Output: a set of couples represented the candidate matches found in the format (tp_<table_from>_<index>,tp_<table_from>_<index>)
     """
-    if not(load_n_best):
-        if not(load_embedding_file):
-            if not(load_graph):
-                df_list = [pd.read_csv(df_list[i]) for i in range(len(df_list))]
-                g = Graph()
-                for i in range(len(df_list)):
-                    g.add_table(df_list[i],str(i), add_table_node=False, link_tuple_token=True, link_token_attribute=True, link_tuple_attribute=False, link_table_tuple=False, link_table_attribute=False,link_table_token=False)
+    
+    if not(load_embedding_file):
+        if not(load_graph):
+            df_list = [pd.read_csv(df_list[i]) for i in range(len(df_list))]
+            g = Graph()
+            for i in range(len(df_list)):
+                g.add_table(df_list[i],str(i), add_table_node=False, link_tuple_token=True, link_token_attribute=True, link_tuple_attribute=False, link_table_tuple=False, link_table_attribute=False,link_table_token=False)
 
-                if file_directory != None:
-                    g.save(directory_name=file_directory)
-            else:
-                g = Graph(directory_name=file_directory)
-                print('Graph loaded')
-        print(f'Number of walks: {g.get_number_of_nodes()}')
-        if not(load_embedding_file):
-            e = Embeddings(g)
-            e.generateNode2vecEmbeddings(n_epochs=n_epochs, p=p, q=q, walk_length=walk_length,embedding_dim=embedding_size, context_size=context_size)
             if file_directory != None:
-                e.save_embedding_tensor(directory_name=file_directory)
+                g.save(directory_name=file_directory)
         else:
-            e = Embeddings(directory_name=file_directory)
-            print('Embeddings loaded')
-
-        tables_to_tuples = get_tuple_indexes_by_table(e.token_to_index)
-        index_list = []
-        for k,v in tables_to_tuples.items():
-            index_list+=v
-        n_top_pairs = find_top_n_faiss(index_list, index_list, e.embeddings, n_similar)
-        n_top_pairs = {index_list[i]:[index_list[t] for t in n_top_pairs[i]] for i in range(n_top_pairs.shape[0])}
-        #n_top_pairs = {tables_to_tuples['A'][i]:[tables_to_tuples['B'][t] for t in n_top_A[i]] for i in range(n_top_A.shape[0])}
-
-        index_to_token = e.index_to_token
+            g = Graph(directory_name=file_directory)
+            print('Graph loaded')
+    print(f'Number of walks: {g.get_number_of_nodes()}')
+    if not(load_embedding_file):
+        e = Embeddings(g)
+        e.generateNode2vecEmbeddings(n_epochs=n_epochs, p=p, q=q, walk_length=walk_length,embedding_dim=embedding_size, context_size=context_size)
         if file_directory != None:
-            save_n_top(file_directory, n_top_A, n_top_B, e.index_to_token)
-    if file_directory != None:
-        n_top_A, n_top_B, index_to_token = load_n_top(file_directory)
+            e.save_embedding_tensor(directory_name=file_directory)
+    else:
+        e = Embeddings(directory_name=file_directory)
+        print('Embeddings loaded')
+
+    tables_to_tuples = get_tuple_indexes_by_table(e.token_to_index)
+    index_list = []
+    for k,v in tables_to_tuples.items():
+        index_list+=v
+    n_top_pairs = find_top_n_faiss(index_list, index_list, e.embeddings, n_similar)
+    # for i in range(len(n_top_pairs)):
+    #     for j in range(len(n_top_pairs[i])):
+    #         if n_top_pairs[i][j] <= i:
+    #             n_top_pairs[i][j] = -1
+    n_top_pairs = {index_list[i]:[index_list[t] for t in n_top_pairs[i] ] for i in range(n_top_pairs.shape[0])}
+    #n_top_pairs = {tables_to_tuples['A'][i]:[tables_to_tuples['B'][t] for t in n_top_A[i]] for i in range(n_top_A.shape[0])}
+
+    index_to_token = e.index_to_token
+
     
     matches = find_matching_couples_free(n_top_pairs,n_top_pairs, index_to_token, n_top)
 
@@ -109,13 +108,15 @@ def free_entity_resolution(df_list,file_directory=None,n_epochs=100,p=20, q=1, n
         tot_comp = 1
         for i in range(len(df_list)):
             tot_comp*=df_list[i].shape[0]
-        print(f'Old number of candidates: {tot_comp}\nNew number of candidates: {len(set(matches[1]))}')
+        print(f'Old number of candidates: {tot_comp}\nNew number of candidates: {len(matches)}')
     except:
         print('Reduction ratio computation failed')
-    return set(matches[1])
+    return matches
 
 if __name__ == '__main__':
     #matches = one_to_one_clean_ER(r"C:\Users\frapu\Desktop\ProgettoBeneventano\Tests\FZ\Datasets\fodors_zagats-tableA.csv", r"C:\Users\frapu\Desktop\ProgettoBeneventano\Tests\FZ\Datasets\fodors_zagats-tableB.csv")
-    matches = free_entity_resolution([r"C:\Users\frapu\Desktop\ProgettoBeneventano\Tests\FZ\Datasets\fodors_zagats-tableA.csv", r"C:\Users\frapu\Desktop\ProgettoBeneventano\Tests\FZ\Datasets\fodors_zagats-tableB.csv"], n_epochs=1)
+    #matches = free_entity_resolution([r"C:\Users\frapu\Desktop\ProgettoBeneventano\Tests\FZ\Datasets\fodors_zagats-tableA.csv", r"C:\Users\frapu\Desktop\ProgettoBeneventano\Tests\FZ\Datasets\fodors_zagats-tableB.csv"], n_epochs=1)
     #print(matches)
+    matches = free_entity_resolution([r"C:\Users\frapu\Desktop\ProgettoBeneventano\Datasets\testAB.csv", r"C:\Users\frapu\Desktop\ProgettoBeneventano\Datasets\testBC.csv"], n_epochs=1)
+    print(matches)
     print('end')
